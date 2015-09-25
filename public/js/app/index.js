2aee602b91c468694c1f2756f5d881d5b6c5786f
vhnam <vhnam2504@gmail.com>
2015-09-26 00:55:46 +0700

    User can talk yourself.

--- public/js/app/index.js
+++ public/js/app/index.js
@@ -1,7 +1,10 @@
-'use strict';
 (function() {
+
+'use strict';
+
 angular.module('serious-chat', ['ngMaterial'])
 
+	// Replace symbol, prevent duplicate with Handlebars
 	.config(function($interpolateProvider){
 		$interpolateProvider
 			.startSymbol('[[')
@@ -9,9 +12,31 @@ angular.module('serious-chat', ['ngMaterial'])
 		;
 	})
 
+	// Cache template
+	.run(function($templateCache) {
+		$templateCache.put('message.html',
+			'<div class="chat-post">' +
+                '<img ng-src="img/{{avatar}}" alt="{{nickname}}" class="chat-avatar" ng-hide="isme">' +
+                '<span class="post-content" ng-class="isme">' +
+                    '<h4 class="post-nickname" ng-hide="isme">{{nickname}}</h4>' +
+                    '{{content}}' +
+                '</span>' +
+            '</div>' +
+            '<div class="clear"></div>'
+		);
+	})
+
+	// Socket.io service
 	.factory('socket', ['$rootScope', function ($rootScope) {
 		var socket = io.connect();
 		return {
+
+			/**
+			 * Subscribe event
+			 *
+			 * @param  {string} eventName
+			 * @param  {function} callback
+			 */
 			on: function (eventName, callback) {
 				function wrapper() {
 					var args = arguments;
@@ -26,6 +51,14 @@ angular.module('serious-chat', ['ngMaterial'])
 					socket.removeListener(eventName, wrapper);
 				}
 			},
+
+			/**
+			 * Publish event
+			 *
+			 * @param  {string} eventName
+			 * @param  {object} data
+			 * @param  {function} callback
+			 */
 			emit: function (eventName, data, callback) {
 				socket.emit(eventName, data, function () {
 					var args = arguments;
@@ -39,31 +72,81 @@ angular.module('serious-chat', ['ngMaterial'])
 		}
 	}])
 
-	.controller('mainController', ['$scope', '$mdSidenav', 'socket', function($scope, $mdSidenav, socket) {
+	.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                            scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+	})
 
+
+	// Message directive
+	.directive('message', function() {
+		return {
+			restrict: 'AE',
+			scope: {
+				avatar: '@',
+				nickname: '@',
+				isme: '@',
+				content: '@'
+			},
+			templateUrl: 'message.html'
+		}
+	})
+
+
+	// Main Controller
+	.controller('mainController', ['$scope', '$mdSidenav', 'socket', function($scope, $mdSidenav, socket) {
 		$scope.user = {};
 
-		$scope.message = '';
+		// Messages of users
+		$scope.messages = [];
 
+		/**
+		 * Show search form
+		 *
+		 */
 		$scope.search = function() {
 			$scope.isSearching = true;
 			$('#search-keyword').show().focus();
 		};
 
+		/**
+		 * Hide search form
+		 *
+		 */
 		$scope.back = function() {
 			$scope.isSearching = false;
 			$('#search-keyword').hide();
 		}
 
-		$scope.showMenu = function() {
+		/**
+		 * Show sidenav
+		 *
+		 */
+		$scope.showSidenav = function() {
 			$mdSidenav('aside-menu').toggle();
 		}
 
 		/**
-		 * function send message
+		 * Send message
 		 */
 		$scope.sendMessage = function(){
-
+			$scope.messages.push({
+				user: {
+					avatar: ($scope.user.avatar) ? $scope.user.avatar : 'anonymous-face.png',
+					nickname: $scope.user.nickname,
+					isme: 'me'
+				},
+				content: $scope.message
+			});
+			$scope.message = '';
 		}
 
 
@@ -83,12 +166,15 @@ angular.module('serious-chat', ['ngMaterial'])
 
 	}])
 
+	// Aside menu controller
 	.controller('asideMenuController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
-		$scope.closeMenu = function() {
-			$mdSidenav('aside-menu').close();
-		};
 
-		$scope.move = function(item) {
+		/**
+		 * Redirect when user clicked on menu
+		 *
+		 * @param  {string}
+		 */
+		$scope.redirect = function(item) {
 			if ('Sign out' === item) {
 				window.location.href = 'app/signout';
 			}
